6fa058571ee755f9d5c09b241a78bea77814c69f
Mohamed Hamdy <99550272+MohamedHamada13@users.noreply.github.com>
2026-04-06 00:11:47 +0200

    Feat: Add IProduct interface & Add and implement product.service & Delete home component

--- src/app/components/home/home.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.html',
-  styleUrl: './home.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class Home {}
